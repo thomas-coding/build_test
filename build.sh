245e898f17f1a1235770f2a71664edb9bd66132c
Jinping Wu <jinping.wu@verisilicon.com>
2022-01-06 15:05:06 +0800

    add ninja

--- build.sh
+++ build.sh
@@ -21,11 +21,19 @@ function gcc_bare_build()
     ./main_g
 }
 
-# make build, we can use make and defile makefile build target
+# make build, we can use make and define makefile to build target
 # http://8.210.111.180/share/doc/compile/make.pdf
 function make_build()
 {
     make -f make/makefile
+    ./main
+}
+
+# ninja build, we can use ninja and define ninja to build target
+# https://ninja-build.org/manual.html
+function ninja_build()
+{
+    ninja  -f ninja/build.ninja
     ./main
 }
 
@@ -34,7 +42,8 @@ cmd_help()
     echo "Basic build:"
     echo "$0 h         ---> command help"
     echo "$0 gcc       ---> use gcc bare build"
-    echo "$0 make       ---> use make build"
+    echo "$0 make      ---> use make build"
+    echo "$0 make      ---> use ninja build"
     echo "$0 c         ---> clean"
 }
 
@@ -44,10 +53,14 @@ elif [[ $1  = "gcc" ]]; then
     gcc_bare_build
 elif [[ $1  = "make" ]]; then
     make_build
+elif [[ $1  = "ninja" ]]; then
+    ninja_build
 elif [[ $1  = "c" ]]; then
     rm -rf main.o sub.o
     rm -rf main main_b main_g
     make clean -f make/makefile
+    ninja -f ninja/build.ninja -t clean
+    rm -rf .ninja_deps .ninja_log
 else
 	echo "wrong args."
 	cmd_help
